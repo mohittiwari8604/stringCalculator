95bde13a06b989fabaa5cd64cf4610ff1ab7eea5
Mohit Tiwari <mohittiwari@MacBook-Air.local>
2025-03-11 11:14:00 +0530

    More test cases included

--- stringCalculator.js
+++ stringCalculator.js
@@ -1,16 +1,27 @@
 function calculate(str){
-    if(str===""){
-        return 0;
-    }
-    const arr=str.split(",")
-    let sum =0 
-    arr.forEach(element => {
-        if(typeof Number(element) === "number"){
-            sum +=Number(element)
-        }
-      
-    });
-    return sum;
-}
+    if(str==="") return 0;
+    // console.log(str.length===1 && typeof Number(str[0])==="number" ,str[0])
+    if(str.length===1 && typeof Number(str[0])==="number" ) return Number(str[0])
 
+    let delimiter = /,|\n/;
+
+    const customDelimiterMatch = str.match(/^\/\/(\[.*?\]|.)\n/);
+    if (customDelimiterMatch) {
+        const delimiterSection = customDelimiterMatch[1];
+        str = str.slice(customDelimiterMatch[0].length); 
+
+        
+        const multipleDelimiters = delimiterSection.match(/\[([^\[\]]+)\]/g);
+        if (multipleDelimiters) {
+            delimiter = new RegExp(multipleDelimiters.map(d => d.slice(1, -1)).join("|"), "g");
+        } else {
+            delimiter = new RegExp(delimiterSection, "g");
+        }
+    }
+
+    const arr=str.split(delimiter).map(n=>Number(n));
+   const sum = arr.reduce((acc,curr)=>acc+curr,0)
+   return sum;
+}
+calculate("4")
 module.exports =calculate;
--- stringCalculator.test.js
+++ stringCalculator.test.js
@@ -8,4 +8,16 @@ describe("Test suite for String Calculator",()=>{
     test("should return 6",()=>{
         expect(calculate("1,2,3")).toBe(6)
     })
+
+    test("Should return number if only one number passed",()=>{
+        expect(calculate("4")).toBe(4)
+    })
+
+    test("should ignore new line and return sum of numbers",()=>{
+        expect(calculate("1\n2,3")).toBe(6)
+    })
+    test("should ignore all other characters which are not number and return sum",()=>{
+        expect(calculate('//[***]\n1***2***3')).toBe(6);
+    })
+
 })
