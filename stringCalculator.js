2a57ee3041ba7c795d96d67281cf639356a360f8
Mohit Tiwari <mohittiwari@MacBook-Air.local>
2025-03-11 10:46:43 +0530

    Comma seperated string sum with tests

--- stringCalculator.js
+++ stringCalculator.js
@@ -2,6 +2,15 @@ function calculate(str){
     if(str===""){
         return 0;
     }
+    const arr=str.split(",")
+    let sum =0 
+    arr.forEach(element => {
+        if(typeof Number(element) === "number"){
+            sum +=Number(element)
+        }
+      
+    });
+    return sum;
 }
 
 module.exports =calculate;
--- stringCalculator.test.js
+++ stringCalculator.test.js
@@ -5,4 +5,7 @@ describe("Test suite for String Calculator",()=>{
     test("Should return 0 when empty string is passed",()=>{
         expect(calculate("")).toBe(0);
     })
+    test("should return 6",()=>{
+        expect(calculate("1,2,3")).toBe(6)
+    })
 })
